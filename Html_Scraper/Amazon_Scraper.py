96ec051d863cd2aa68bccc4430ec1cb68725e676
Deepakpant9759 <deepakpant9759@gmail.com>
2025-08-08 09:54:05 +0530

    price finded

--- Html_Scraper/Amazon_Scraper.py
+++ Html_Scraper/Amazon_Scraper.py
@@ -12,13 +12,23 @@ request_headers = {
 def get_page_html(url):
     res = requests.get(url=url,headers=request_headers)
     return res.content
+
+def get_product_price(soup):
+    main_price_sapn = soup.find('span',class_ ="a-price-whole").get_text()
+    return(main_price_sapn)
+
 def extract_product_info(url):
+    product_info={}
     print(f"Extract the data of {url}")
     html = get_page_html(url=url)
-    return html
+    soup = bs4.BeautifulSoup(html,'html.parser')
+    product_info['Price'] = get_product_price(soup)
+    return product_info
+
+
 
 if __name__ == "__main__":
-    with open(r"D:\Programing Playground\WebScrapying_Scratch\Html_Scraper\Amazon_products.csv",newline='') as csvfile:
+    with open(r"C:\Users\MICILMEDS\Battel_with_Codes\WebScrapying_Scratch\Html_Scraper\Amazon_products.csv",newline='') as csvfile:
         reader = csv.reader(csvfile,delimiter=",")
         for rw in reader:
             url = rw[0]
