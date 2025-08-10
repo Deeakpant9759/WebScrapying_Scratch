54c2c349adfefabf0c003433f5ecc1587454ac2b
Deepakpant9759 <deepakpant9759@gmail.com>
2025-08-10 17:08:01 +0530

    New things added

--- Html_Scraper/Amazon_Scraper.py
+++ Html_Scraper/Amazon_Scraper.py
@@ -2,7 +2,7 @@ from datetime import datetime
 import requests
 import bs4
 import csv
-
+import pandas as pd
 useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36"
 request_headers = {
     "User-Agent": useragent,
@@ -17,25 +17,56 @@ def get_product_price(soup):
     # Price (full, with ₹)
     container = soup.find('div',class_ = 'a-section a-spacing-none aok-align-center aok-relative')
     if container:
-        target_span = container.find('span',class_='aok-offscreen')
+        target_span = container.find('span',class_='a-price-whole')
         if target_span:
-            text_value = target_span.get_text(strip=True)
+            text_value = target_span.get_text(strip=True).replace(',','')
+            try:
+                return float(text_value)
+            except ValueError:
+                print('Value count not be parsed')
+                return
+def get_title_of_product(soup):
+    container = soup.find('div',id="titleSection")
+    if container:
+        target_span = container.find('span',id="productTitle")
+        if target_span:
+            text_value = target_span.text.strip()
             return text_value
-    
+def get_rating(soup):
+    container = soup.find('div',id="averageCustomerReviews")
+    if container:
+        target_span = container.find('span',class_="a-icon-alt")
+        if target_span:
+            text_value = target_span.text.strip()
+            return text_value
+def get_Product_Tech(soup):
+    details = {}
+    container = soup.find('div',id="prodDetails")
+    data_Tech = soup.findAll('table',class_="a-keyvalue prodDetTable")
+    for data_Table in data_Tech:
+        table_Rows = data_Table.findAll('tr')
+        for row in table_Rows:
+            row_key = row.find('th').text.strip()
+            row_value = row.find('td').text.strip()
+            details[row_key] = row_value
+    return details
 
 def extract_product_info(url):
     product_info={}
-    print(f"Extract the data of {url}")
     html = get_page_html(url=url)
     soup = bs4.BeautifulSoup(html,'html.parser')
     product_info['Price'] = get_product_price(soup)
+    product_info['Title'] = get_title_of_product(soup)
+    product_info['rating']=get_rating(soup)
+    product_info.update(get_Product_Tech(soup))
     return product_info
-
-
-
 if __name__ == "__main__":
-    #with open(r"C:\Users\MICILMEDS\Battel_with_Codes\WebScrapying_Scratch\Html_Scraper\Amazon_products.csv",newline='') as csvfile:
-        #reader = csv.reader(csvfile,delimiter=",")
-        #for rw in reader:
-            url = 'https://www.amazon.in/Redmi-Note-14-5G-Dimensity/dp/B0DPFTYQN7/ref=sr_1_1_sspa?crid=3GMG52BTVYP0G&dib=eyJ2IjoiMSJ9.WFxCmg4fW900Jb-ghJ-WzcqbOex1V1yH1ZD5sgNjt2f5tkhHACiJI2VV-eC2EW4KtAe-P7E03QAvt09KwC6KFnxygo8GD-au57hsFnQ2ytrHGcz3GJsHf71FJlcD9Moa9KTBfmnm7OktC80tO__98F5_7EZcLTjR7cyAnLxUgB7GopRsiwrv3SmkW3RyNmIQW8Ys4DtpCKY_aEhOXiyD_6o5l0sp5A1FYu2MvXp_BXI.vVHuptBpdjbovg59qjcyRrzF9SKwZ5grgHvQhm1jXJQ&dib_tag=se&keywords=best%2Bphone%2Bunder%2B20k&qid=1754729718&sprefix=best%2Bphone%2B%2Caps%2C229&sr=8-1-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&th=1'
-            print(extract_product_info(url))
+    with open(r"C:\Users\MICILMEDS\Battel_with_Codes\WebScrapying_Scratch\Html_Scraper\Amazon_products.csv", newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=",")
+        for rw in reader:
+            url = rw[0]  # Take first column of the row
+            print("------------------------------------------Data Extracted--------------------------------------")
+            df = pd.DataFrame([extract_product_info(url)])
+            print(df)
+           
+
